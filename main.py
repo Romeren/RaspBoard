0e73eba26ccd94500ffec0ecdd802349d27c59ee
Romeren <emilromer@hotmail.com>
2018-01-24 10:13:10 +0100

    her

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 # -*- coding: utf-8 -*-  # NOQA
-from service_framework.plugin_module import Plugin_module as framework  # NOQA
+from RaspBoard.service_framework.plugin_module import Plugin_module as framework  # NOQA
 
 # plugins:
 plugins = []
 
-from services.misc.restserver import config as java  # NOQA
+from RaspBoard.services.misc.restserver import config as java  # NOQA
 plugins.append(java)
 
 # tiles
-from services.tiles.clock.restservice import config as clock  # NOQA
+from RaspBoard.services.tiles.clock.restservice import config as clock  # NOQA
 plugins.append(clock)
-from services.tiles.slideshow.restservice import config as slide  # NOQA
+from RaspBoard.services.tiles.slideshow.restservice import config as slide  # NOQA
 plugins.append(slide)
 
 # pages
-from services.pages.dashboard.restservice import config as dashboard  # NOQA
+from RaspBoard.services.pages.dashboard.restservice import config as dashboard  # NOQA
 plugins.append(dashboard)
 
 # chrome cast hook
-from services.pages.casthook.restservice import config as cast  # NOQA
+from RaspBoard.services.pages.casthook.restservice import config as cast  # NOQA
 plugins.append(cast)
 
 # API's
-from services.apis.random_image.restservice import config as rnd
+from RaspBoard.services.apis.random_image.restservice import config as rnd
 plugins.append(rnd)
 
 system_settings = {'port': 8080}
