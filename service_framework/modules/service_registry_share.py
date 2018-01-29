a80228a4a52ad2307c0373afb56afd9abe84e868
Romeren <emroe@ramboll.dk>
2018-01-29 13:05:17 +0100

    adding remote services

--- service_framework/modules/service_registry_share.py
+++ service_framework/modules/service_registry_share.py
@@ -9,16 +9,18 @@ class Service(superClass):
         self.module.add_event_listener('SERVICE_REGISTRY', self.add_remote_services)
 
     def share_services(self, event):
+        if(event.origin_host != self.module.ip_address):
+            return
         services = self.module.get_services('.*')
         serialized = []
         for service in services:
             s = service.copy()
             s.pop('handler', None)
             serialized.append(s)
-        self.module.dispatch_event('SERVICE_REGRISTRY', serialized)
+        self.module.dispatch_event('SERVICE_REGISTRY', serialized)
 
     def add_remote_services(self, event):
-        if(evelt.origin_host == self.module.ip_address):
+        if(event.origin_host == self.module.ip_address):
             return
         for config in event.data:
             self.module.add_service(config)
